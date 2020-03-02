4ca92b9ab78aa9eda7247b5941f9e0a95e58d277
xuetingyan <xuetingyan@u.northwestern.edu>
2020-03-02 16:23:36 -0600

    200228 2w analysis done

--- 23w_method/2w_measurement_analysis.py
+++ 23w_method/2w_measurement_analysis.py
@@ -9,19 +9,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-fname = '200227//200227_glass_R78_R1516_2w_measurement_3.txt'
+#fname = '200227//200227_glass_R78_R1516_2w_measurement_3.txt'
+fname = "200228//200228_glass_R78_R1516_2w_measurement_5.txt"
 fig, axs = plt.subplots(1,2, figsize=(8,4))
 graph_data = pd.read_csv(fname, sep = ' ', header = 0,
-                         names = ['Date_time','Time','TC','SENS','Lockin1f','Lockin2f','X1','Y1','X2','Y2'])
+                         names = ['Date_time','Time','TC','SENS','Lockin1f','Lockin2f','X2','Y2','X1','Y1'])
   
-axs[0].scatter(graph_data.Lockin1f, graph_data.X2, label = 'X2')
-axs[0].scatter(graph_data.Lockin2f, graph_data.Y2, label = 'Y2')
+axs[0].scatter(graph_data.Lockin1f, -graph_data.X2, label = 'X2')
+axs[0].scatter(graph_data.Lockin2f, -graph_data.Y2, label = 'Y2')
 #axs[0].plot(graph_data.Lockin1f, graph_data.X2)
 #axs[0].plot(graph_data.Lockin2f, graph_data.Y2)
 axs[0].set_xlabel('f(Hz)')
 axs[0].set_ylabel('Real_V3w(V)')
 axs[0].set_xscale('log')
-axs[0].set_ylim([-100e-6, 200e-6])
+axs[0].set_ylim([-100e-6, 300e-6])
 #    axs[0].set_xscale('log')
 axs[0].legend(loc = 'upper right')
 
@@ -36,3 +37,4 @@ axs[1].set_ylabel('Imag_V3w(V)')
 axs[1].legend(loc = 'upper right')
 
 plt.tight_layout()
+plt.savefig("200228//200228_glass_R78_R1516_2w_measurement_5_plot", dpi = 300)
