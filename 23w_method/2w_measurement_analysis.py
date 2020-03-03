eb39ad0ff7ae77b84036d60b466cfe629abc87c8
xuetingyan <xuetingyan@u.northwestern.edu>
2020-03-03 11:06:21 -0600

    200302 2w analysis done

--- 23w_method/2w_measurement_analysis.py
+++ 23w_method/2w_measurement_analysis.py
@@ -10,13 +10,13 @@ import pandas as pd
 
 
 #fname = '200227//200227_glass_R78_R1516_2w_measurement_3.txt'
-fname = "200228//200228_glass_R78_R1516_2w_measurement_5.txt"
+fname = "200302//200302_glass_R65_R1817_2w_measurement_1.txt"
 fig, axs = plt.subplots(1,2, figsize=(8,4))
 graph_data = pd.read_csv(fname, sep = ' ', header = 0,
                          names = ['Date_time','Time','TC','SENS','Lockin1f','Lockin2f','X2','Y2','X1','Y1'])
   
-axs[0].scatter(graph_data.Lockin1f, -graph_data.X2, label = 'X2')
-axs[0].scatter(graph_data.Lockin2f, -graph_data.Y2, label = 'Y2')
+axs[0].scatter(graph_data.Lockin1f, graph_data.X2, label = 'X2')
+axs[0].scatter(graph_data.Lockin2f, graph_data.Y2, label = 'Y2')
 #axs[0].plot(graph_data.Lockin1f, graph_data.X2)
 #axs[0].plot(graph_data.Lockin2f, graph_data.Y2)
 axs[0].set_xlabel('f(Hz)')
@@ -37,4 +37,4 @@ axs[1].set_ylabel('Imag_V3w(V)')
 axs[1].legend(loc = 'upper right')
 
 plt.tight_layout()
-plt.savefig("200228//200228_glass_R78_R1516_2w_measurement_5_plot", dpi = 300)
+plt.savefig("200302//200302_glass_R65_R1817_2w_measurement_1_plot", dpi = 300)
