1997a8a1cc7617476fd48a473c9dbfdb061810fe
xuetingyan <xuetingyan@u.northwestern.edu>
2020-06-24 11:40:43 -0500

    minor fix of suspended wire power dep test

--- suspended_wire/power_dependence_test.py
+++ suspended_wire/power_dependence_test.py
@@ -11,25 +11,8 @@ import visa
 import numpy as np
 import os
 
-### basic parameters ###
-#os.mkdir('200204')
-TESTNAME = "200204//200204_P_4_power_dep_f3p4_test5" #record the frequency
-rm = visa.ResourceManager();
-print(rm.list_resources())
-#fg = rm.open_resource("GPIB::9::INSTR")
-lockin1 = rm.open_resource("GPIB2::9::INSTR") #sample & SINE_OUT source
-lockin2 = rm.open_resource("GPIB2::18::INSTR") #reference resistor
-
-### output file initialize ###
-FILENAME = TESTNAME + '_' + str(datetime.now()).replace(':','-') + ".txt"
-
-t0 = time.time()
-ti = datetime.now()
-header = "Date_time Time V_input X1 Y1 X1_ref Y1_ref X3 Y3 X3_ref Y3_ref\n"
-with open(FILENAME,'w') as output:
-    output.write(header)
-
-### lock in initialize ###
+########### function defitions ################################################
+### lock in 1w initialize ###
 def lockinInit_1w():
     #set lockin1 to internal (1), lockin2 to external(0)
     #lockin1.write("FMOD 0")
@@ -37,28 +20,23 @@ def lockinInit_1w():
     #set lockins to measure the 1w voltage
     lockin1.write("HARM 1")
     lockin2.write("HARM 1")
-    #make phases zero
-    lockin1.write("PHAS 0")
-    lockin2.write("PHAS 0")
-    #set input configuration
-    lockin1.write("ISRC 1")
-    lockin2.write("ISRC 1")
-    #ground type
-    lockin1.write("IGND 1")
-    lockin2.write("IGND 1")
-    #input coupling
-    lockin1.write("ICPL 1")
-    lockin2.write("ICPL 1")
-    #reserve mode
-    lockin1.write("RMOD 1")
-    lockin2.write("RMOD 1")
-    #sensitivity
-    #lockin1.write("SENS 22")
-    #lockin2.write("SENS 22")
-    #time constant
-    #lockin1.write("OFLT 9")
-    #lockin2.write("OFLT 9")
+#    #make phases zero
+#    lockin1.write("PHAS 0")
+#    lockin2.write("PHAS 0")
+#    #set input configuration
+#    lockin1.write("ISRC 1")
+#    lockin2.write("ISRC 1")
+#    #ground type
+#    lockin1.write("IGND 1")
+#    lockin2.write("IGND 1")
+#    #input coupling
+#    lockin1.write("ICPL 1")
+#    lockin2.write("ICPL 1")
+#    #reserve mode
+#    lockin1.write("RMOD 1")
+#    lockin2.write("RMOD 1")
 
+### lock in 3w initialize ###
 def lockinInit_3w():
     #set lockins to measure the 3w voltage
     lockin1.write("HARM 3")
@@ -111,21 +89,42 @@ def VoltageSweep(voltages,sens1, TC1, SENS1, initWaitTime1, sens3, TC3, SENS3, i
         print(str(datetime.now()) + " " + str(t) + " " + line)
         with open(FILENAME,'a') as output:
             output.write(str(datetime.now()) + " " + str(t) + " " + line +"\n")
+ ##############################################################################
 
+### crate a folder with today's date and create a new file name ###
+date = '200624'
+try:
+    os.mkdir(date)
+except FileExistsError:
+    pass
+FILENAME = f"{date}//{date}_Bi2Te30617_power_dep_f3p4.txt"
 
+rm = visa.ResourceManager();
+print(rm.list_resources())
+lockin1 = rm.open_resource("GPIB2::9::INSTR") #sample & SINE_OUT source
+lockin2 = rm.open_resource("GPIB2::18::INSTR") #reference resistor
+t0 = time.time()
+ti = datetime.now()
+header = "Date time Time V_input X1 Y1 X1_ref Y1_ref X3 Y3 X3_ref Y3_ref\n"
+with open(FILENAME,'w') as output:
+    output.write(header)
+
+### Set the parameters ###
 freq = 3.4 #Hz
-lockin1.write('FREQ %f' %freq)
-voltages = np.array([2.2, 2.3, 2.4, 2.554, 2.6, 2.7])
-sens1 = 1e-3#allowed error
-timeCon1 = 10#time const for 1w measurement
+voltages = np.array([0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3])
+sens1 = 1e-3#allowed error for 1w
+timeCon1 = 13#time const for 1w measurement
 sensitivity1 = 24#sensitivity for 1w measurement
-initWaitTime1 = 60 #s
+initWaitTime1 = 150 #s
 
-sens3 = 1e-6#allowed error
-timeCon3 = 11#time const for 3w measurement
-sensitivity3 = 15#sensitivity for 3w measurement
+sens3 = 1e-6#allowed error for 3w
+timeCon3 = 13#time const for 3w measurement
+sensitivity3 = 14#sensitivity for 3w measurement
 initWaitTime3 = 300 #s
-VoltageSweep(voltages, sens1, timeCon1, sensitivity1, initWaitTime1, sens3, timeCon3, sensitivity3, initWaitTime3)
+
+lockin1.write('FREQ %f' %freq)
+VoltageSweep(voltages, sens1, timeCon1, sensitivity1, initWaitTime1,
+            sens3, timeCon3, sensitivity3, initWaitTime3)
 
 lockin1.write("SLVL %f" %0.004)
 lockinInit_1w()
