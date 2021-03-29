d982c4432f4bf8ec1bc55e90f0f3d743120cf3a4
xuetingyan <xuetingyan@u.northwestern.edu>
2021-03-29 16:49:06 -0500

    Bi2Te3 p5 in air analysis update

--- 23w_method/2w_measurement_analysis.py
+++ 23w_method/2w_measurement_analysis.py
@@ -9,13 +9,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-fname = "200303//200303_glass_R43_R2019_2w_measurement_1.txt"
-fig, axs = plt.subplots(1,2, figsize=(8,4))
+fname = "201218//201218_glass_R87_R1516_2w_measurement_dry_ice.txt"
 graph_data = pd.read_csv(fname, sep = ' ', header = 0,
-                         names = ['Date_time','Time','TC','SENS','Lockin1f','Lockin2f','X2','Y2','X1','Y1'])
+                         names = ['Date_time','Time','TC','SENS','Lockin1f','Lockin2f','X1','Y1','X2','Y2'])
 V1w = np.average(graph_data['X1'].head())
-X = -graph_data.X2
-Y = -graph_data.Y2
+X = graph_data.X2
+Y = graph_data.Y2
 R = np.sqrt(X**2 + Y**2)
 theta = np.arctan(Y/ X)
 theta2 = theta.copy()
@@ -30,32 +29,32 @@ result = pd.DataFrame({'freq': graph_data.Lockin1f, 'X2': X, 'Y2' : Y, 'R' : R,
                        'modified X2' : R * np.cos(theta2  - 90 / 180 * np.pi), 
                        'modified Y2' : R * np.sin(theta2  - 90 / 180 * np.pi)})
 result.to_csv(fname[:len(fname)-4] + ".csv", index = False)
-  
-axs[0].scatter(graph_data.Lockin1f, R * np.cos(theta2  - 90 / 180 * np.pi), label = 'X2')
-axs[0].scatter(graph_data.Lockin2f, R * np.sin(theta2  - 90 / 180 * np.pi), label = 'Y2')
+#fig, axs = plt.subplots(1,2, figsize=(8,4))
+#axs[0].scatter(graph_data.Lockin1f, R * np.cos(theta2  - 90 / 180 * np.pi), label = 'X2')
+#axs[0].scatter(graph_data.Lockin2f, R * np.sin(theta2  - 90 / 180 * np.pi), label = 'Y2')
 #axs[0].scatter(graph_data.Lockin1f, X, label = 'X2')
 #axs[0].scatter(graph_data.Lockin2f, Y, label = 'Y2')
 
 #axs[0].plot(graph_data.Lockin1f, graph_data.X2)
 #axs[0].plot(graph_data.Lockin2f, graph_data.Y2)
-axs[0].set_xlabel('f(Hz)')
-axs[0].set_ylabel('V2w(V)')
-axs[0].set_xscale('log')
-axs[0].set_ylim([-300e-6, 300e-6])
-#    axs[0].set_xscale('log')
-axs[0].legend(loc = 'upper right')
-
-axs[1].scatter(graph_data.Lockin1f, graph_data.X1, label = 'X1')
-axs[1].scatter(graph_data.Lockin2f, graph_data.Y1, label = 'Y1')
-axs[1].plot(graph_data.Lockin1f, graph_data.X1)
-axs[1].plot(graph_data.Lockin2f, graph_data.Y1)
-
-axs[1].set_xlabel('f(Hz)')
-axs[1].set_ylabel('V1w(V)')
-#    axs[1].set_xscale('log')
-axs[1].legend(loc = 'upper right')
-
-plt.tight_layout()
+#axs[0].set_xlabel('f(Hz)')
+#axs[0].set_ylabel('V2w(V)')
+#axs[0].set_xscale('log')
+#axs[0].set_ylim([-300e-6, 300e-6])
+##    axs[0].set_xscale('log')
+#axs[0].legend(loc = 'upper right')
+#
+#axs[1].scatter(graph_data.Lockin1f, graph_data.X1, label = 'X1')
+#axs[1].scatter(graph_data.Lockin2f, graph_data.Y1, label = 'Y1')
+#axs[1].plot(graph_data.Lockin1f, graph_data.X1)
+#axs[1].plot(graph_data.Lockin2f, graph_data.Y1)
+#
+#axs[1].set_xlabel('f(Hz)')
+#axs[1].set_ylabel('V1w(V)')
+##    axs[1].set_xscale('log')
+#axs[1].legend(loc = 'upper right')
+#
+#plt.tight_layout()
 
 #plt.savefig(fname[:len(fname)-4] + "_phase_-90", dpi = 300)
 
--- 23w_method/fitting_2w_1.m
+++ 23w_method/fitting_2w_1.m
@@ -3,7 +3,9 @@
 %define k matrix and other parameters
 %fname = '200228\200228_glass_R78_R1516_2w_measurement_5.csv';
 %fname = '200302\200302_glass_R65_R1817_2w_measurement_1.csv';
-fname = '200303\200303_glass_R43_R2019_2w_measurement_1.csv';
+%fname = '200303\200303_glass_R43_R2019_2w_measurement_1.csv';
+%fname = '201215\201215_glass_R87_R1516_2w_measurement.csv';
+fname = '201218\201218_glass_R87_R1516_2w_measurement_dry_ice.csv';
 data = readtable(fname);
 switch fname
     case '200228\200228_glass_R78_R1516_2w_measurement_5.csv'
@@ -24,11 +26,26 @@ switch fname
         Vdc = 0.3841;
         Idc = Vdc / Rt;
         V1w = 0.4621;
+     
+    case '201215\201215_glass_R87_R1516_2w_measurement.csv'
+        Rh = 37.89;
+        Rt = 38.05;
+        Vdc = 0.30768;
+        Idc = Vdc / Rt;%0.008030745;
+        V1w = 0.48881;
+        alpha = 0.002140;
+    case '201218\201218_glass_R87_R1516_2w_measurement_dry_ice.csv'
+        Rh = 32.7294;%31.9231;%32.7294;
+        Rt = 32.8676;%32.0579;%32.8676;
+        Vdc = 0.27386;
+        Idc = Vdc / Rt;%0.30768 / 38.05 * (998.139+38.05) / (998.139+32.8676); %Vdc / Rt;%0.009531809;
+        V1w = 0.45417;
+        alpha = 0.002128;
     otherwise
         disp('invalid case');
 end
 Lh =  1.83e-3;%2.15e-3; %m %-------------estimated, need to check autocad
-alpha = (0.002015 + 0.002005 + 0.001989 + 0.001988) / 4;
+%alpha = (0.002015 + 0.002005 + 0.001989 + 0.001988) / 4;
 % Rh = 39.75;%38.86;%40.20;%39.75;
 % Rt = 39.92;%39.79;%40.27;%39.92;
 % Vdc = 0.3861;%0.3841;%0.3882;%0.3861;
@@ -39,14 +56,16 @@ data.T2wX = data.modifiedX2 * Rh * Lh * sqrt(2) / (V1w^2 * Rt * alpha * Idc);
 data.T2wY = data.modifiedY2 * Rh * Lh * sqrt(2) / (V1w^2 * Rt * alpha * Idc);
 %data.T2wX = data.modifiedX2 * sqrt(2) / (Rt * alpha * Idc);
 %data.T2wY = data.modifiedY2 * sqrt(2) / (Rt * alpha * Idc);
-T2wcombo = [data.T2wX ; data.T2wY];
+T2wcombo = [data.T2wX ; data.T2wY]./ 1.13;
 freqcombo = [data.freq; data.freq];
 % fo = fitoptions('Method','NonlinearLeastSquares',...
 %                'Lower',[0,0,0],...
 %                'Upper',[10,10,10],...
 %                'StartPoint',[1,1,1]);
 ft = fittype( 'anisotropicT2w(x, sdetk, p0, gamma2)', 'problem', 'gamma2');
-gamma2s = 0.8 : 0.001 : 0.9;
+%[f,gof] = fit(freqcombo, T2wcombo, ft, 'StartPoint', [1,1], 'problem', 1);
+%gamma2s = 0.8 : 0.01 : 0.9;
+gamma2s = 0.9 : 0.01: 1.2;
 Fits = cell(1, length(gamma2s));
 Gofs = cell(1, length(gamma2s));
 
@@ -62,15 +81,20 @@ for i = 1 : 1 : length(gamma2s)
     sdetks(i) = Fits{i}.sdetk;
     p0s(i) = Fits{i}.p0;
 end
+
+fz = 16;
 f2 = figure;
 plot(gamma2s, sdetks, 'DisplayName', 'sdetk');
 hold on
 plot(gamma2s, p0s, 'DisplayName', 'p0');
 hold off
-legend('show')
+lg = legend('show');
+lg.FontSize = fz;
 ax = gca;
 ax.XLabel.String = 'Value of gamma2';
+ax.XLabel.FontSize = fz;
 ax.YLabel.String = 'Value of fitting parameter';
+ax.YLabel.FontSize = fz;
 
 f1 = figure;
 plot(freqcombo, T2wcombo,'b.', 'MarkerSize', 6)
@@ -84,7 +108,9 @@ legend('data', 'real fit', 'imag fit')
 ax = gca;
 ax.XScale = 'log';
 ax.XLabel.String = 'f(Hz)';
+ax.XLabel.FontSize = fz;
 ax.YLabel.String = 'T2w(K)';
+ax.YLabel.FontSize = fz;
 % fparam = [detk, p0];
 % result = [fparam; confintf];
 % str = sprintf('Coefficients (with 95%% confidence bounds):\ndetk = %0.3f, (%0.3f, %0.3f)\np0 = %0.3f, (%0.3f, %0.3f)', result(:));
